b974235b5e0c17643111510c7e2b32e07261717b
Isaiah Hamilton <isaiah7hamilton@gmail.com>
2022-07-23 20:19:52 -0400

    add ANSI text

--- nvim/.config/nvim/init.lua
+++ nvim/.config/nvim/init.lua
@@ -1,3 +1,14 @@
+--
+-- ███╗   ██╗██╗   ██╗██╗███╗   ███╗
+-- ████╗  ██║██║   ██║██║████╗ ████║
+-- ██╔██╗ ██║██║   ██║██║██╔████╔██║
+-- ██║╚██╗██║╚██╗ ██╔╝██║██║╚██╔╝██║
+-- ██║ ╚████║ ╚████╔╝ ██║██║ ╚═╝ ██║
+-- ╚═╝  ╚═══╝  ╚═══╝  ╚═╝╚═╝     ╚═╝
+--
+-- isaiah's neovim configuration
+-- https://github.com/isaiah-hamilton
+
 require("mappings")
 require("options")
 require("plugins")
