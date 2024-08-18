7c58367047952ae2efcb7815454063a76a8e6147
Isaiah Hamilton <isaiah7hamilton@gmail.com>
2024-08-18 01:05:23 -0400

    update colorscheme to catppuccin

--- .config/nvim/lua/plugins/colorscheme.lua
+++ .config/nvim/lua/plugins/colorscheme.lua
@@ -1,11 +1,11 @@
 return {
-  { "sainnhe/gruvbox-material" },
+  { "catppuccin/nvim", name = "catppuccin", priority = 1000, opts = { transparent_background = true } },
 
   -- Configure LazyVim to load gruvbox
   {
     "LazyVim/LazyVim",
     opts = {
-      colorscheme = "gruvbox-material",
+      colorscheme = "catppuccin",
     },
   },
 }
--- .config/wezterm/wezterm.lua
+++ .config/wezterm/wezterm.lua
@@ -1,14 +1,17 @@
--- Pull in the wezterm API
 local wezterm = require("wezterm")
-
--- This will hold the configuration.
 local config = wezterm.config_builder()
 
+-- font
 config.font = wezterm.font("JetBrainsMono Nerd Font")
+config.font_size = 12
 
+-- color scheme
+config.color_scheme = "Catppuccin Mocha"
+
+-- terminal window
 config.enable_tab_bar = false
 config.window_decorations = "RESIZE"
-config.window_background_opacity = 0.8
+config.window_background_opacity = 0.85
 config.macos_window_background_blur = 10
 
 return config
